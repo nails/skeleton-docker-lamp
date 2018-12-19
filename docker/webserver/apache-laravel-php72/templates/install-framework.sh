8f8524f098850afe9942c8bb02b12e205f804f5e
Pablo de la Peña <p@shedcollective.org>
2018-12-19 12:37:50 +0000

    chore: checking for existence of laravel installer

--- docker/webserver/apache-laravel-php72/templates/install-framework.sh
+++ docker/webserver/apache-laravel-php72/templates/install-framework.sh
@@ -6,8 +6,10 @@ echo "+----------------------------+"
 
 # --------------------------------------------------------------------------
 
-echo "... installing Laravel installer"
-composer global require "laravel/installer"
+if ! [ -x "$(command -v laravel)" ]; then
+    echo "... installing Laravel installer"
+    composer global require "laravel/installer"
+fi
 
 echo "... installing Laravel"
 laravel new www --force
