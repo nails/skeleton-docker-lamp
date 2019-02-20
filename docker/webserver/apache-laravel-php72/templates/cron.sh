ddeb0651f75ede6c58d3f47fdbeb677d1aff9cab
Pablo de la Peña <p@shedcollective.org>
2019-02-20 18:23:56 +0000

    Getting cron and rsys log runnuing properly

--- docker/webserver/apache-laravel-php72/templates/cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chown www-bridge-user:crontab /var/spool/cron/crontabs/www-bridge-user
-chmod 600 /var/spool/cron/crontabs/www-bridge-user
-chown root:crontab /var/spool/cron/crontabs/root
-chmod 600 /var/spool/cron/crontabs/root
-# Writing container vars so that crond picks them up
-# This feels a little dirty, so opinions welcome
-env >> /etc/environment
-service cron start
