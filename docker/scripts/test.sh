7acf9d88d079b265d0dadc368cd7cc641951fc75
Pablo de la Peña <p@shedcollective.org>
2019-03-26 14:23:15 +0100

    Updated bash syntax

--- docker/scripts/test.sh
+++ docker/scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ -f ./www/scripts/test.sh ]; then
+if [[ -f ./www/scripts/test.sh ]]; then
     docker-compose exec --user=1000:1000 webserver ./scripts/test.sh ${1}
 fi
--- docker/scripts/up.sh
+++ docker/scripts/up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run the installer if the ./www directory is not there
-if [ ! -d ./www ]; then
+if [[ ! -d ./www ]]; then
     mkdir -p www
     INSTALLER=$(cat docker-compose.yml | sed -n 's/ *build: "\(docker\/webserver\/.*\)"/\1/p');
     INSTALLER_PATH="$INSTALLER/templates/install-framework.sh"
-    if [ -f $INSTALLER_PATH ]; then
+    if [[ -f $INSTALLER_PATH ]]; then
         ./"$INSTALLER_PATH"
     fi
 fi
@@ -40,7 +40,7 @@ make ssl-create
 # --------------------------------------------------------------------------
 
 # If there is no vendor folder then execute a build
-if [ ! -d ./www/vendor ]; then
+if [[ ! -d ./www/vendor ]]; then
     echo ""
     echo "No vendor folder detected; building project for first time"
     echo ""
