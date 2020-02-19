cee29dfda11e0dca424808754b2e5e0108d6ee11
Pablo de la Peña <p@shedcollective.org>
2020-02-19 15:22:47 +0000

    Various
    - Adds exit codes for failed commands during `make up`
    - Installing NodeJS 12
    - Properly exposing select env vars to Apache
    - Configuring OPCache
    - Installing and configuring ModPageSpeed

--- docker/scripts/up.sh
+++ docker/scripts/up.sh
@@ -18,7 +18,7 @@ docker-compose up -d || exit 1
 # --------------------------------------------------------------------------
 
 # Start rsylog
-docker-compose exec webserver bash -c "service rsyslog restart"
+docker-compose exec webserver bash -c "service rsyslog restart" || exit 1
 
 # --------------------------------------------------------------------------
 
@@ -26,27 +26,33 @@ docker-compose exec webserver bash -c "service rsyslog restart"
 echo "Installing crontabs"
 
 # Dump the env vars so we can use it in cron
-docker-compose exec --user=1000:1000 webserver bash -c "printenv > ~/env.sh"
+docker-compose exec --user=1000:1000 webserver bash -c "printenv > ~/env.sh" || exit 1
 # Ensure they are prefixed with `export`
-docker-compose exec --user=1000:1000 webserver bash -c "sed -iE 's/^\(.*\)=\(.*\)$/export \1=\"\2\"/g' ~/env.sh"
+docker-compose exec --user=1000:1000 webserver bash -c "sed -iE 's/^\(.*\)=\(.*\)$/export \1=\"\2\"/g' ~/env.sh" || exit 1
 # Escape nested quotes - replace all quotes with \"
-docker-compose exec --user=1000:1000 webserver bash -c "sed -iE 's/\"/\\\\\"/g' ~/env.sh"
+docker-compose exec --user=1000:1000 webserver bash -c "sed -iE 's/\"/\\\\\"/g' ~/env.sh" || exit 1
 # Unescape the surrounding VAR="" quotes
 # NotE: For some reason the `-i` flag doesn't work with this regex so piping and redirecting
-docker-compose exec --user=1000:1000 webserver bash -c "cat ~/env.sh | sed -E 's/^(.+)=\\\\\"(.*)\\\\\"/\1=\"\2\"/g' > ~/env.tmp && mv ~/env.tmp ~/env.sh"
+docker-compose exec --user=1000:1000 webserver bash -c "cat ~/env.sh | sed -E 's/^(.+)=\\\\\"(.*)\\\\\"/\1=\"\2\"/g' > ~/env.tmp && mv ~/env.tmp ~/env.sh" || exit 1
 
 # Repeat for root
-docker-compose exec webserver bash -c "printenv > ~/env.sh"
-docker-compose exec webserver bash -c "sed -iE 's/^\(.*\)=\(.*\)$/export \1=\"\2\"/g' ~/env.sh"
-docker-compose exec webserver bash -c "sed -iE 's/\"/\\\\\"/g' ~/env.sh"
-docker-compose exec webserver bash -c "cat ~/env.sh | sed -E 's/^(.+)=\\\\\"(.*)\\\\\"/\1=\"\2\"/g' > ~/env.tmp && mv ~/env.tmp ~/env.sh"
+docker-compose exec webserver bash -c "printenv > ~/env.sh" || exit 1
+docker-compose exec webserver bash -c "sed -iE 's/^\(.*\)=\(.*\)$/export \1=\"\2\"/g' ~/env.sh" || exit 1
+docker-compose exec webserver bash -c "sed -iE 's/\"/\\\\\"/g' ~/env.sh" || exit 1
+docker-compose exec webserver bash -c "cat ~/env.sh | sed -E 's/^(.+)=\\\\\"(.*)\\\\\"/\1=\"\2\"/g' > ~/env.tmp && mv ~/env.tmp ~/env.sh" || exit 1
 
 # Install the crontabs
-docker-compose exec --user=1000:1000 webserver bash -c "cat ~/crontab | crontab -"
-docker-compose exec webserver bash -c "cat ~/crontab | crontab -"
+docker-compose exec --user=1000:1000 webserver bash -c "cat ~/crontab | crontab -" || exit 1
+docker-compose exec webserver bash -c "cat ~/crontab | crontab -" || exit 1
 
 # Restart cron
-docker-compose exec webserver bash -c "service cron restart"
+docker-compose exec webserver bash -c "service cron restart" || exit  1
+
+# --------------------------------------------------------------------------
+
+# Set the Apache envvars
+docker-compose exec webserver bash -c 'echo "export DOMAIN=\"${DOMAIN}\"" >> /env-apache.sh' || exit 1
+docker-compose exec webserver bash -c 'echo "export PAGESPEED_ENABLE=\"${PAGESPEED_ENABLE}\"" >> /env-apache.sh' || exit 1
 
 # --------------------------------------------------------------------------
 
