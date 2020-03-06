be174703bd43b85ed2f002874ae0d7d0ca389cc1
Pablo de la Peña <p@shedcollective.org>
2020-03-06 15:07:19 +0000

    Using Docker Hub for db and backup containers

--- docker/backup/templates/backup-directory.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-shed project:backup:directories \
-    -n \
-    --domain=$DOMAIN \
-    --s3-key=$S3_ACCESS_KEY \
-    --s3-secret=$S3_ACCESS_SECRET \
-    --s3-bucket=$S3_BUCKET \
-    --directory=$DIRECTORY \
